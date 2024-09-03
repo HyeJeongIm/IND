1dd9e4e63e2d85014d6746858e4cc9e1255654a4
ImHyeJeong <dlaowjde@naver.com>
2024-09-03 17:29:30 +0900

    add: IND buffer test

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import numpy as np
 import torch
 from sklearn.metrics import roc_auc_score
 from tqdm import tqdm
+
 def eval(net, c, dataloader, device):
 
     scores = []
@@ -21,3 +22,12 @@ def eval(net, c, dataloader, device):
             indices.extend(list(range(i * dataloader.batch_size, (i + 1) * dataloader.batch_size)))
 
     return indices, labels, scores
+
+def eval_single_image(net, c, image, device):
+    net.eval()
+    with torch.no_grad():
+        image = image.float().to(device)
+        z = net(image)
+        dist = torch.sum((z - c) ** 2, dim=1)
+        score = dist.cpu().numpy()
+    return score
