bd80f249eaaf7f2625aa628b6b3d4d701b10a452
ImHyeJeong <dlaowjde@naver.com>
2024-09-11 21:26:42 +0900

    change: threshold setting&update with KDE, visualize min/max/most radius with circle

--- IND_buffer.py
+++ IND_buffer.py
@@ -1,19 +1,17 @@
-import numpy as np
-import argparse 
-import torch
-
-from train import TrainerDeepSVDD
-# from preprocess import get_mnist
-from dataloader import get_dataloaders
-from dataloader_IND import get_IND_loaders, load_min_max, CustomDatasetLoader, get_only_IND_testloaders
 import os
-import visualization
-from test import eval, eval_single_image, eval, eval_single_image_IDPOC
-from torchvision import transforms
-from utils.utils import global_contrast_normalization
-from torch.utils.data import DataLoader
 import random
-import ipdb
+import torch
+import argparse 
+import numpy as np
+import visualization
+from train import TrainerDeepSVDD
+from torchvision import transforms
+from torch.utils.data import DataLoader
+from sklearn.neighbors import KernelDensity
+from utils.utils import global_contrast_normalization
+from test import eval, eval_single_image, eval, eval_single_image_IDPOC
+from dataloader_IND import get_IND_loaders, load_min_max, CustomDatasetLoader, get_only_IND_testloaders
+
 def set_seed(seed):
     """Set the random seed for reproducibility."""
     random.seed(seed)
@@ -39,6 +37,7 @@ if __name__ == '__main__':
     parser.add_argument("--batch_size", type=int, default=1, help="Batch size")
     parser.add_argument('--pretrain', type=bool, default=True, help='Whether to pretrain the network using an autoencoder')
     parser.add_argument('--latent_dim', type=int, default=128, help='Dimension of the latent variable z')
+    
     # Dataset
     parser.add_argument('--dataset_path', type=str, default='./data/MVTecAD', help='Path to the dataset root directory')
     # parser.add_argument('--dataset_name', type=str, default='leather', choices=['leather', 'metal_nut', 'wood'], help='Name of the dataset to use')
@@ -51,26 +50,28 @@ if __name__ == '__main__':
     parser.add_argument('--normal_class', type=int, default=0, help='Class to be treated as normal. The rest will be considered as anomalous.') 
     
     args = parser.parse_args()
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
- 
     set_seed(args.seed)
 
-    # normal class 
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    
+    """
+        - 초기 class 및 normal class 정의 
+        - 초기 threshold 설정 
+            - normal data의 most, max의 평균값
+    """
     normal_classes = ['good']  
-    # novel classes
     class_names = ['leather', 'metal_nut', 'wood']
 
-    # 각 novel class의 threshold 설정
     thresholds = {
-        'leather': 102.45,
-        'metal_nut': 36.15,
-        'wood': 32.12
+        'leather': 22.82,
+        'metal_nut': 10.88,
+        'wood': 32.22
     }
     
-    # new instance
+    # new instance class 정의 
     instance_folders = ['glue', 'color', 'cut', 'fold', 'poke']
     for instance in instance_folders:
-        args.dataset_name = 'leather'
+        args.dataset_name = 'leather' # 수정 필요 
         print(f"Processing {instance} dataset...")
         
         novel_buffer = []
@@ -80,16 +81,19 @@ if __name__ == '__main__':
         new_instance = [instance]
         normal_classes.append(instance)
 
-        # normal + novel(new instance) dataset
+        """ 
+            Load Data 
+            - get_IND_loaders: normal + novel(new instance) dataset
+            - get_only_IND_testloaders: only novel(new instance) dataset
+        """
         data_NI = get_IND_loaders(args, normal_classes)  
-        # only novel(new instance) dataset
         data_only_NI = get_only_IND_testloaders(args, new_instance) 
         _, data_only_NI_loader = data_only_NI
         
         for idx, (image, label) in enumerate(data_only_NI[1]):
             image = image.to(device)
             scores = {}
-            is_novel = True  # 이미지가 모든 모델에서 novel로 분류되는지 여부
+            is_novel = True  # 이미지가 모든 모델에서 novel로 분류되는지 여부 확인 가능 
 
             for class_name in class_names:
                 """ Create Model & Load weight """
@@ -123,7 +127,10 @@ if __name__ == '__main__':
 
             results.append((idx, label[0].item(), scores, novelty_status))
         
-        # Save results to file
+        """
+            Save result
+            - new instance에 대해서만 Known or Novel 구분한 결과
+        """
         results_dir = os.path.join(args.output_path, "IND_buffer", args.testdataset_version, instance)
         os.makedirs(results_dir, exist_ok=True)  
    
@@ -132,21 +139,28 @@ if __name__ == '__main__':
             for idx, label, scores, novelty_status in results:
                 min_class_score = min(scores.values())  
                 f.write(f"Image {idx + 1} - Label: {label}, Scores: {scores}, Status: {novelty_status}, Min Score: {min_class_score:.2f}\n")
+            
+            # known_buffer와 novel_buffer에 담긴 이미지 개수 추가
+            f.write(f"\nTotal Known Images: {len(known_buffer)}\n")
+            f.write(f"Total Novel Images: {len(novel_buffer)}\n")
+
+        # Save known/novel images
+        visualization.visualize_and_save_known_images(known_buffer, results_dir)
+        visualization.visualize_and_save_novel_images(novel_buffer, results_dir)
 
         print(f"Initial results saved to {results_path}")
 
-        # Save novel images
-        visualization.visualize_and_save_novel_images(novel_buffer, results_dir)
-        
+
         """ 
             2) OOD update 
-                - novel_buffer에 novel image가 있는 경우, update 진행
-
+                - novel_buffer에 novel image가 있는 경우
+                  1) update 진행 (fine tuning)
+                  2) threshold update
         """
         # Fine-tune if "Novel"
         if len(novel_buffer) > 0:
             print(f"Fine-tuning with {len(novel_buffer)} Novel images from {instance}")
-            args.dataset_name = 'leather'
+            args.dataset_name = 'leather' # 수정 필요 
             min_max = load_min_max(args.dataset_name, args.dataset_path)
 
             transform = transforms.Compose([
@@ -169,9 +183,13 @@ if __name__ == '__main__':
             # fine-tuning
             buffer_fine_tune_model.buffer_fine_tune()
 
-            """ Test (Dataset: Known + Novel) """
-            # Re-evaluate on the test set after fine-tuning
-            fine_tune_results_dir = os.path.join(args.output_path, "IND_buffer", f"{args.testdataset_version}_fine_tune", instance)
+            """ 
+                Test 
+                - normal + new instance를 normal로 fine tuning 했을 때 다양한 결과 
+            """
+            # fine_tune_results_dir = os.path.join(args.output_path, "IND_buffer", f"{args.testdataset_version}_fine_tune", instance)
+            fine_tune_results_dir = os.path.join(args.output_path, "IND_buffer", f"{args.testdataset_version}_fine_tune_th_kde", instance)
+
             os.makedirs(fine_tune_results_dir, exist_ok=True)
 
             # Load fine-tuned model weights
@@ -181,29 +199,29 @@ if __name__ == '__main__':
             fine_tune_model.c = torch.Tensor(state_dict['center']).to(fine_tune_model.device)
             
             indices, labels, scores = eval(fine_tune_model.net, fine_tune_model.c, data_NI[1], device)
-            
-            normal_count = 0
-            anomaly_count = 0
-            
-            for label in labels:
-                if label == 0:
-                    normal_count += 1
-                elif label == 1:
-                    anomaly_count += 1
-            
-            # 각 instance에 대한 레이블의 개수 출력
-            print(f"Instance: {instance}")
-            print(f"  Number of normal (label 0) samples: {normal_count}")
-            print(f"  Number of anomaly (label 1) samples: {anomaly_count}")
 
-            """ Visualization """
+            """ Calculate threshold """
             # Seperate normal and abnormal score
             normal_scores = [score for label, score in zip(labels, scores) if label == 0] # normal
             abnormal_scores = [score for label, score in zip(labels, scores) if label == 1] # abnormal
             normal_scores_np = np.array(normal_scores)
-            mean_normal_scores_np = np.mean(normal_scores_np)
-            thresholds[args.dataset_name] = mean_normal_scores_np
+            
+            # max R 
+            max_radius = np.max(normal_scores_np)
+            
+            # KDE를 사용하여 반지름의 밀도 추정
+            kde = KernelDensity(kernel='gaussian', bandwidth=0.1).fit(normal_scores_np[:, None])
+            log_density = kde.score_samples(normal_scores_np[:, None])
+            density = np.exp(log_density)
+            # 가장 밀도가 높은 반지름 찾기
+            most_common_radius = normal_scores_np[np.argmax(density)]
 
+            """ threshold 계산 및 update """
+            # threshold_update_value = (max_radius + most_common_radius) / 2
+            # thresholds[args.dataset_name] = threshold_update_value
+            thresholds[args.dataset_name] = most_common_radius
+
+            """ Visualization """
             # Score Distribution
             visualization.distribution_normal(normal_scores, fine_tune_results_dir)
             visualization.distribution_abnormal(abnormal_scores, fine_tune_results_dir)
@@ -218,5 +236,8 @@ if __name__ == '__main__':
             visualization.visualize_feature_embeddings(fine_tune_model.net, data_NI[1], fine_tune_results_dir, device)
             visualization.plot_roc_curve(labels, scores, fine_tune_results_dir)
             visualization.plot_feature_distribution(fine_tune_model.net, data_NI[1], fine_tune_results_dir, device)
-
-     
+            visualization.plot_feature_distribution(fine_tune_model.net, data_NI[1], fine_tune_results_dir, device)
+            
+            # min, max, most common radius에 따른 circle
+            visualization.visualize_latent_space_train(fine_tune_model.net, fine_tune_model.c, data_NI[0], device, fine_tune_results_dir)
+            visualization.visualize_latent_space_test(fine_tune_model.net, fine_tune_model.c, data_NI[1], device, fine_tune_results_dir)
